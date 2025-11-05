c022eea37c4b2f14393bb22895bb45a7f3ca7b8d
Tarjei <tarjeisvendsen@outlook.com>
2025-11-05 19:00:43 +0100

    Added basic necessary files

---
+++ src/main/java/tari/socialsonic/SocialSonicApplication.java
@@ -0,0 +1,13 @@
+package tari.socialsonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSonicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialSonicApplication.class, args);
+    }
+
+}
