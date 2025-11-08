64a53489f67d3b33336e18c50cd1565f8b57da2b
Tarjei <tarjeisvendsen@outlook.com>
2025-11-08 01:09:07 +0100

    Created helper enum and methods for error code handling and creation

---
+++ src/main/java/tari/socialsonic/utils/errors/ErrorCodes.java
@@ -0,0 +1,81 @@
+package tari.socialsonic.utils.errors;
+
+import tari.socialsonic.SubsonicResponse;
+
+public enum ErrorCodes {
+    GENERIC(0,"A generic error."),
+    MISSING_PARAMETER(10,"Required parameter is missing."),
+    CLIENT_SUB_VERSION(20,"Incompatible Subsonic REST protocol version. Client must upgrade."),
+    SERVER_SUB_VERSION(30,"Incompatible Subsonic REST protocol version. Server must upgrade."),
+    AUTH_FAILED_CLASSIC(40,"Wrong username or password."),
+    LDAP_TOKEN_NOT_SUPPORTED(41,"Token authentication not supported for LDAP users."),
+    NON_SUPPORTED_AUTH(42,"Provided authentication mechanism not supported."),
+    CONFLICTION_AUTHS(43,"Multiple conflicting authentication mechanisms provided."),
+    INVALID_API_KEY(44,"Invalid API key."),
+    NOT_AUTHORIZED(50,"User is not authorized for the given operation."),
+    SUBSONIC_PREM(60,"The trial period for the Subsonic server is over. Please upgrade to Subsonic Premium. Visit subsonic.org for details."),
+    NOT_FOUND(70,"The requested data was not found.");
+
+    public final int code;
+    public final String description;
+
+    ErrorCodes(int code,String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public static SubsonicResponse createErrorResponse(ErrorCodes errorCode){
+        switch (errorCode){
+            case MISSING_PARAMETER -> {
+                return new SubsonicResponse(constructErrorAttributes(MISSING_PARAMETER.code, MISSING_PARAMETER.description, "https://example.com"));
+            }
+            case CLIENT_SUB_VERSION -> {
+                return new SubsonicResponse(constructErrorAttributes(CLIENT_SUB_VERSION.code, CLIENT_SUB_VERSION.description, "https://example.com"));
+            }
+            case SERVER_SUB_VERSION -> {
+                return new SubsonicResponse(constructErrorAttributes(SERVER_SUB_VERSION.code, SERVER_SUB_VERSION.description, "https://example.com"));
+
+            }
+            case AUTH_FAILED_CLASSIC -> {
+                return new SubsonicResponse(constructErrorAttributes(AUTH_FAILED_CLASSIC.code, AUTH_FAILED_CLASSIC.description, "https://example.com"));
+
+            }
+            case LDAP_TOKEN_NOT_SUPPORTED -> {
+                return new SubsonicResponse(constructErrorAttributes(LDAP_TOKEN_NOT_SUPPORTED.code, LDAP_TOKEN_NOT_SUPPORTED.description, "https://example.com"));
+
+            }
+            case NON_SUPPORTED_AUTH -> {
+                return new SubsonicResponse(constructErrorAttributes(NON_SUPPORTED_AUTH.code, NON_SUPPORTED_AUTH.description, "https://example.com"));
+
+            }
+            case CONFLICTION_AUTHS -> {
+                return new SubsonicResponse(constructErrorAttributes(CONFLICTION_AUTHS.code, CONFLICTION_AUTHS.description, "https://example.com"));
+
+            }
+            case INVALID_API_KEY -> {
+                return new SubsonicResponse(constructErrorAttributes(INVALID_API_KEY.code, INVALID_API_KEY.description, "https://example.com"));
+
+            }
+            case NOT_AUTHORIZED -> {
+                return new SubsonicResponse(constructErrorAttributes(NOT_AUTHORIZED.code, NOT_AUTHORIZED.description, "https://example.com"));
+            }
+            case SUBSONIC_PREM -> {
+                return new SubsonicResponse(constructErrorAttributes(SUBSONIC_PREM.code, SUBSONIC_PREM.description, "https://example.com"));
+
+            }
+            case NOT_FOUND -> {
+                return new SubsonicResponse(constructErrorAttributes(NOT_FOUND.code, NOT_FOUND.description, "https://example.com"));
+
+            }
+            default -> {
+                return new SubsonicResponse(constructErrorAttributes(GENERIC.code, GENERIC.description, "https://example.com"));
+            }
+        }
+    }
+
+    public static SubsonicResponse.Attribute[] constructErrorAttributes(int code, String message, String helpUrl){
+        return new SubsonicResponse.Attribute[]{new SubsonicResponse.Attribute("code",code),
+                new SubsonicResponse.Attribute("message",message),
+                new SubsonicResponse.Attribute("helpUrl",helpUrl)};
+    }
+}
