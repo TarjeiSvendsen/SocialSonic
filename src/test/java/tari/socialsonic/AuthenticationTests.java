a97f9865407928142d19af133a43aa663bba3a27
Tarjei <tarjeisvendsen@outlook.com>
2025-12-06 15:35:22 +0100

    Added more tests to cover the auth utils.

---
+++ src/test/java/tari/socialsonic/AuthenticationTests.java
@@ -0,0 +1,90 @@
+package tari.socialsonic;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import tari.socialsonic.utils.auth.AuthenticationUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Tag("UnitTests")
+@ExtendWith(MockitoExtension.class)
+public class AuthenticationTests {
+
+    @Mock
+    AuthenticationUtils utils;
+
+    @Nested
+    class ApiKeyTests{
+        @Test
+        public void authInvalid(){
+            Map<String,String> params = new HashMap<>();
+            params.put("apiKey","123");
+            params.put("u","helloThere");
+            assertFalse(utils.authenticate(params),"Method should return invalid given the circumstance of both an api key and user");
+        }
+    }
+    @Nested
+    class UserPasswordTests{
+        @Test
+        public void legacyNotSupported(){
+            Map<String,String> params = new HashMap<>();
+            params.put("u","helloThere");
+            params.put("p","nope");
+            assertFalse(utils.authenticate(params),"Legacy authentication with a plaintext password is not supported");
+        }
+
+        @Test
+        public void missingUserParam(){
+            Map<String,String> params = new HashMap<>();
+            params.put("p","nope");
+            assertFalse(utils.authenticate(params),"Legacy authentication with a plaintext password is not supported");
+        }
+        @Test
+        public void missingPasswordParam(){
+            Map<String,String> params = new HashMap<>();
+            params.put("u","helloThere");
+
+            assertFalse(utils.authenticate(params),"Legacy authentication with a plaintext password is not supported");
+        }
+        @Test
+        public void missingTokenParam(){
+            Map<String,String> params = new HashMap<>();
+            params.put("u","helloThere");
+            params.put("s","bingingSalt");
+            assertFalse(utils.authenticate(params),"Legacy authentication with a plaintext password is not supported");
+        }
+        @Test
+        public void missingSaltParam(){
+            Map<String,String> params = new HashMap<>();
+            params.put("u","helloThere");
+            params.put("t","h3h44h45");
+            assertFalse(utils.authenticate(params),"Legacy authentication with a plaintext password is not supported");
+        }
+
+
+    }
+    @Nested
+    class GenerationTests{
+
+        @Test
+        public void apiKeyNotNull(){
+            assertNotNull(AuthenticationUtils.generateKey());
+        }
+        @Test
+        public void apiKeyNoLongerThan(){
+            String result = AuthenticationUtils.generateKey();
+
+            assertFalse(()-> result.length() > 64);
+            assertTrue(()->result.length() > 8 );
+        }
+    }
+}
