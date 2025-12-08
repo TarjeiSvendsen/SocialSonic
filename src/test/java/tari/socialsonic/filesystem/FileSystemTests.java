afeca853ff31cd6dfde0e6efc4f48c8ff369718f
Tarjei <tarjeisvendsen@outlook.com>
2025-12-08 18:21:49 +0100

    Added tests for scanner.

---
+++ src/test/java/tari/socialsonic/filesystem/FileSystemTests.java
@@ -0,0 +1,18 @@
+package tari.socialsonic.filesystem;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import tari.socialsonic.utils.filesystem.Scanner;
+
+
+public class FileSystemTests {
+    @Nested
+    class FileMetadataTests{
+
+        @Test
+        void checkFileHeader(){
+
+            Scanner.scanRoot();
+        }
+    }
+}
