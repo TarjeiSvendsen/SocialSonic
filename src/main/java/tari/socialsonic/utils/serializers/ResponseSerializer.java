73f3f64c8239db561fb262b053e0d76798a46dd1
Tarjei <tarjeisvendsen@outlook.com>
2026-03-11 19:42:19 +0100

    Actually wrote comments so I don't have to reimplement the whole thing next time... + a quick optimization.

--- src/main/java/tari/socialsonic/utils/serializers/ResponseSerializer.java
+++ src/main/java/tari/socialsonic/utils/serializers/ResponseSerializer.java
@@ -12,6 +12,14 @@ import java.util.Map;
 
 public class ResponseSerializer extends ValueSerializer<SubsonicResponse> {
 
+    /**
+     *
+     * @param subsonicResponse Value to serialize; can <b>not</b> be null.
+     * @param jsonGenerator Generator used to output resulting JSON content
+     * @param ctxt Context that can be used to get serializers for
+     *   serializing Objects value contains, if any.
+     * @throws JacksonException Handled automatically by Spring Boot
+     */
     @Override
     public void serialize(SubsonicResponse subsonicResponse, JsonGenerator jsonGenerator, SerializationContext ctxt) throws JacksonException {
         if (jsonGenerator instanceof ToXmlGenerator xmlGenerator) {
@@ -22,12 +30,12 @@ public class ResponseSerializer extends ValueSerializer<SubsonicResponse> {
         }
         else {
             jsonGenerator.writeStartObject();
-
+            // This is here because we need a root node.
             jsonGenerator.writeName("subsonic-response");
             jsonGenerator.writeStartObject();
 
             writeAttributes(subsonicResponse, jsonGenerator);
-            writeChildNodes(subsonicResponse, jsonGenerator);
+            if (!subsonicResponse.childNodes.isEmpty()) writeChildNodes(subsonicResponse, jsonGenerator);
 
             jsonGenerator.writeEndObject();
             jsonGenerator.writeEndObject();
@@ -38,9 +46,17 @@ public class ResponseSerializer extends ValueSerializer<SubsonicResponse> {
         super();
     }
 
+    /**
+     * This writes the attributes from the subsonic objects attributes map.
+     * @param response the {@link SubsonicResponse} object to read attributes from
+     * @param generator the generator, either of type {@link JsonGenerator} or {@link ToXmlGenerator}.
+     */
     private void writeAttributes(SubsonicResponse response,JsonGenerator generator){
+        // Only really necessary in this method, as .setNextIsAttribute is specific to ToXmlGenerator.
         if (generator instanceof ToXmlGenerator xmlGenerator) {
             for (Map.Entry<String,Object> entry: response.attributes.entrySet()){
+                // When writing attributes, writeStartObject is not necessary, as it is an attribute, not a new object.
+                // Why I thought otherwise, idk...
                 xmlGenerator.writeName(entry.getKey());
                 xmlGenerator.setNextIsAttribute(true);
                 xmlGenerator.writeString(entry.getValue().toString());
@@ -53,7 +69,14 @@ public class ResponseSerializer extends ValueSerializer<SubsonicResponse> {
             }
         }
     }
+
+    /**
+     * Writes child nodes recursively for the specified object.
+     * @param response the {@link SubsonicResponse} object to read child nodes from
+     * @param generator the generator, either of type {@link JsonGenerator} or {@link ToXmlGenerator}.
+     */
     private void writeChildNodes(SubsonicResponse response, JsonGenerator generator){
+        // No need to differentiate between the generators in this, as this uses methods available in both.
         for (Map.Entry<String,List<SubsonicResponse>> children: response.childNodes.entrySet()){
             for (SubsonicResponse child : children.getValue()){
                 generator.writeName(children.getKey());
