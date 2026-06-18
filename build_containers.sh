81dac5aa1969a6430c192666bbcb0a3283980ae9
Tarjei <tarjei@kvok.no>
2026-06-18 21:59:04 +0200

    refactor: removed everything to start anew
    This comes from a decision to incorporate new learnings into the project, and also to satisfy the stereotypical need to restart...

--- build_containers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo ./mvnw package
-
-sudo docker build -t mragravaine/socialsonic:large -f Dockerfile .
-
-sudo docker build -t mragravaine/socialsonic:graalvm-upx -f graalvm-upx.Dockerfile .
--- src/main/java/no/tari/socialsonic/SocialSonicApplication.java
+++ src/main/java/no/tari/socialsonic/SocialSonicApplication.java
@@ -1,4 +1,4 @@
-package tari.socialsonic;
+package no.tari.socialsonic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
